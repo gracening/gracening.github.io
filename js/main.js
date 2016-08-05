9c0bf9d23a2793520cdaccf55b3629a43e66d155
Grace Ning <gracening97@gmail.com>
2016-08-05 14:06:46 -0400

    Animate progress bars

--- js/main.js
+++ js/main.js
@@ -1,6 +1,23 @@
-// $("html, body").animate({ scrollTop: $('#title1').height() }, 1000);
-
+//Autoscroll
 $('.link').click(function() {
 	var selected = $($(this).attr('href'));
 	$("html, body").animate({ scrollTop: selected.offset().top -50 }, 1000);
+});
+
+//Animate progress bars
+$(function() {
+	$('#skills').waypoint({	
+		handler: function() {
+			$(".bar > span").each(function() {
+				$(this)
+					.data("origWidth", $(this).data("percent"))
+					//.width(0)
+					.animate({
+						width: $(this).data("origWidth")
+					}, 1200);
+			});
+			this.destroy();
+		},
+		offset: '20%'
+	})
 });
