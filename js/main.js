262f04464cd2932c225978042b5ba547dad77893
Grace Ning <gracening97@gmail.com>
2016-08-05 16:25:17 -0400

    Fix flicker on scrollTop

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 //Autoscroll
-$('.link').click(function() {
+$('.link').click(function(e) {
+	e.preventDefault();
 	var selected = $($(this).attr('href'));
 	$("html, body").animate({ scrollTop: selected.offset().top -50 }, 1000);
 });
