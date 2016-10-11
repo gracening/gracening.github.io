d50cb8d0897d7cbd24aac775d9cb243867201dc3
Grace Ning <gracening97@gmail.com>
2016-10-10 22:25:51 -0400

    Clean up code

--- js/main.js
+++ js/main.js
@@ -4,21 +4,3 @@ $('.link').click(function(e) {
 	var selected = $($(this).attr('href'));
 	$("html, body").animate({ scrollTop: selected.offset().top -50 }, 1000);
 });
-
-//Animate progress bars
-$(function() {
-	$('#skills').waypoint({	
-		handler: function() {
-			$(".bar > span").each(function() {
-				$(this)
-					.data("origWidth", $(this).data("percent"))
-					//.width(0)
-					.animate({
-						width: $(this).data("origWidth")
-					}, 1200);
-			});
-			this.destroy();
-		},
-		offset: '20%'
-	})
-});
