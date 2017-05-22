05b60f878bbdba025022a617fbc26cd53fa0b2e3
Grace Ning <gracening97@gmail.com>
2017-05-22 17:30:00 -0400

    Fix auto scroll

--- js/main.js
+++ js/main.js
@@ -1,14 +1,16 @@
 //Autoscroll
-$('.link').click(function(e) {
-	e.preventDefault();
-	var selected = $($(this).attr('href'));
-	$("html, body").animate({ scrollTop: selected.offset().top -50 }, 1000);
-});
+$(document).ready(function () {
+	$('.link').click(function(e) {
+		e.preventDefault();
+		var selected = $($(this).attr('href'));
+		$("html, body").animate({ scrollTop: selected.offset().top -50 }, 1000);
+	});
 
-$('.art-grid li a').magnificPopup({
-    type: 'image',
-    gallery: {
-    	enabled: true,
-		navigateByImgClick: true
-    }
+	$('.art-grid li a').magnificpopup({
+		type: 'image',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true
+		}
+	});
 });
