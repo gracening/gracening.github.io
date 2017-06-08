5045f5ed8277e835d1fccc9ae1cd1093eb7f86bc
Grace Ning <gracening97@gmail.com>
2017-06-08 01:22:58 -0400

    Animate nav bar

--- js/main.js
+++ js/main.js
@@ -1,11 +1,48 @@
-//Autoscroll
 $(document).ready(function () {
+	var scrolled;
+	var lastScrollTop = 0;
+	var navHeight = $('header').outerHeight();
+
+	// autoscroll
 	$('.link').click(function(e) {
 		e.preventDefault();
 		var selected = $($(this).attr('href'));
 		$("html, body").animate({ scrollTop: selected.offset().top -50 }, 1000);
 	});
+	
+	// nav bar
+	$(window).scroll(function(event){
+		scrolled = true;
+	});
 
+	setInterval(function() {
+		if (scrolled) {
+			moveNav();
+			scrolled = false;
+		}
+	}, 250);
+
+	function moveNav() {
+		var st = $(this).scrollTop();
+		
+		if(Math.abs(lastScrollTop - st) <= 5)
+			return;
+		
+		if (st > lastScrollTop && st > navHeight){
+			// scroll Down
+			$('header').removeClass('nav-down').addClass('nav-up');
+		} 
+		else {
+			// scroll Up
+			if(st + $(window).height() < $(document).height()) {
+				$('header').removeClass('nav-up').addClass('nav-down');
+			}
+		}
+		
+		lastScrollTop = st;
+	}
+	
+	// pop up
 	$('.art-grid li a').magnificPopup({
 		type: 'image',
 		gallery: {
@@ -14,3 +51,4 @@ $(document).ready(function () {
 		}
 	});
 });
+
