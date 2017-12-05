fc0005cd7e57c5d1fc82222d0e9f0126ca8cbd89
Grace Ning <gracening97@gmail.com>
2017-12-05 17:07:50 -0500

    Restyle nav bar

--- js/main.js
+++ js/main.js
@@ -1,54 +1,18 @@
 $(document).ready(function () {
-	var scrolled;
-	var lastScrollTop = 0;
-	var navHeight = $('header').outerHeight();
+  // autoscroll
+  $('.scroll').click(function(e) {
+    e.preventDefault();
+    var selected = $($(this).attr('href'));
+    $("html, body").animate({ scrollTop: selected.offset().top -50 }, 1000);
+  });
 
-	// autoscroll
-	$('.link').click(function(e) {
-		e.preventDefault();
-		var selected = $($(this).attr('href'));
-		$("html, body").animate({ scrollTop: selected.offset().top -50 }, 1000);
-	});
-	
-	// nav bar
-	$(window).scroll(function(event){
-		scrolled = true;
-	});
-
-	setInterval(function() {
-		if (scrolled) {
-			moveNav();
-			scrolled = false;
-		}
-	}, 250);
-
-	function moveNav() {
-		var st = $(this).scrollTop();
-		
-		if(Math.abs(lastScrollTop - st) <= 5)
-			return;
-		
-		if (st > lastScrollTop && st > navHeight){
-			// scroll Down
-			$('header').removeClass('nav-down').addClass('nav-up');
-		} 
-		else {
-			// scroll Up
-			if(st + $(window).height() < $(document).height()) {
-				$('header').removeClass('nav-up').addClass('nav-down');
-			}
-		}
-		
-		lastScrollTop = st;
-	}
-	
-	// pop up
-	$('.art-grid li a').magnificPopup({
-		type: 'image',
-		gallery: {
-			enabled: true,
-			navigateByImgClick: true
-		}
-	});
+  // pop up
+  $('.art-grid li a').magnificPopup({
+    type: 'image',
+    gallery: {
+      enabled: true,
+      navigateByImgClick: true
+    }
+  });
 });
 
